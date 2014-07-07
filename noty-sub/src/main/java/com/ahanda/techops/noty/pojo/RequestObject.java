96a4187e1312931d9eb98df824d280c25957eac9
Gautam Kanwar <Robby@admins-MacBook-Pro.local>
2014-07-07 15:17:37 +0530

    REcfactor

---
+++ noty-sub/src/main/java/com/ahanda/techops/noty/pojo/RequestObject.java
@@ -0,0 +1,24 @@
+package com.ahanda.techops.noty.pojo;
+
+public class RequestObject
+{
+	private String jString;
+
+
+	public RequestObject()
+	{
+		jString = "";
+	}
+
+	public RequestObject(String jString)
+	{
+		try
+		{
+
+		}
+		catch (Exception e)
+		{
+
+		}
+	}
+}
---
+++ noty-sub/src/main/java/com/ahanda/techops/noty/server/PintMessageHandler.java
@@ -0,0 +1,23 @@
+package com.ahanda.techops.noty.server;
+
+import com.ahanda.techops.noty.pojo.RequestObject;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class PintMessageHandler extends SimpleChannelInboundHandler<RequestObject>
+{
+
+	/**
+	 *  This methid will get called when a new request is received
+	 */
+	@Override
+	protected void channelRead0(ChannelHandlerContext arg0, RequestObject msg) throws Exception
+	{
+		
+		
+	}
+
+
+}
