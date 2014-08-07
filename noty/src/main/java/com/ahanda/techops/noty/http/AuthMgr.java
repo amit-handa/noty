cf94d59ecdaf2265b9703454bbc1731d9ce96b8d
Gautam <kanwar.iitd@gmail.com>
2014-08-07 10:18:37 +0530

    Indentation changes

--- noty/src/main/java/com/ahanda/techops/noty/http/AuthMgr.java
+++ noty/src/main/java/com/ahanda/techops/noty/http/AuthMgr.java
@@ -45,13 +45,14 @@ public class AuthMgr extends SimpleChannelInboundHandler<Request>
 	private Mac mac;
 
 	private String secretKey; // $TRDATADIR
-	
+
 	private long validityWindow;
 
 	private Map<String, UserInfo> userInfos = new HashMap<String, UserInfo>();
 
 	private Set<String> invalidSessions = new HashSet<String>();
-	private static Set< Cookie > nocookies = new HashSet< Cookie >();
+
+	private static Set<Cookie> nocookies = new HashSet<Cookie>();
 
 	public AuthMgr()
 	{
@@ -68,22 +69,22 @@ public class AuthMgr extends SimpleChannelInboundHandler<Request>
 		{
 			ObjectNode config = Config.getInstance().get();
 			ObjectNode defconfig = Config.getDefault();
-			String macAlgoName = defconfig.get( NotyConstants.MAC_ALGO_NAME ).asText();
-			
-			JsonNode tmp = config.get( NotyConstants.MAC_ALGO_NAME );
-			if( tmp != null )
+			String macAlgoName = defconfig.get(NotyConstants.MAC_ALGO_NAME).asText();
+
+			JsonNode tmp = config.get(NotyConstants.MAC_ALGO_NAME);
+			if (tmp != null)
 				macAlgoName = tmp.asText();
 
-			validityWindow = defconfig.get("http").get( NotyConstants.HTTP_SESSIONS_VALIDITY ).asLong();
-			tmp = config.get( "http" ).get( NotyConstants.HTTP_SESSIONS_VALIDITY );
-			if( tmp != null )
+			validityWindow = defconfig.get("http").get(NotyConstants.HTTP_SESSIONS_VALIDITY).asLong();
+			tmp = config.get("http").get(NotyConstants.HTTP_SESSIONS_VALIDITY);
+			if (tmp != null)
 				validityWindow = tmp.asLong();
 
-			mac = Mac.getInstance( macAlgoName );
+			mac = Mac.getInstance(macAlgoName);
 
-			secretKey = config.get( NotyConstants.SESS_KEY ).asText();
+			secretKey = config.get(NotyConstants.SESS_KEY).asText();
 
-			SecretKeySpec sks = new SecretKeySpec(secretKey.getBytes(), macAlgoName );
+			SecretKeySpec sks = new SecretKeySpec(secretKey.getBytes(), macAlgoName);
 			mac.init(sks);
 		}
 		catch (Exception exc)
@@ -118,161 +119,175 @@ public class AuthMgr extends SimpleChannelInboundHandler<Request>
 		}
 	}
 
-    public static Map< String, Object > checkCredential( final Mac mac, final Map< String, Object > msg ) {
-		String userId = (String)msg.get( "userId" );
-		String password = (String)msg.get( "password" );
+	public static Map<String, Object> checkCredential(final Mac mac, final Map<String, Object> msg)
+	{
+		String userId = (String) msg.get("userId");
+		String password = (String) msg.get("password");
 
 		String sessId = null;
 
-		if( password != null ) {
+		if (password != null)
+		{
 			long sessStart = System.currentTimeMillis() / 1000L;
-			sessId = getSessId( mac, userId, sessStart );
-			Map< String, Object > reply = new HashMap< String, Object >();
-			reply.put( "userId", userId );
-			reply.put( "sessStart", sessStart );
-			reply.put( "sessId", sessId );
-			reply.put( "status", "ok" );
+			sessId = getSessId(mac, userId, sessStart);
+			Map<String, Object> reply = new HashMap<String, Object>();
+			reply.put("userId", userId);
+			reply.put("sessStart", sessStart);
+			reply.put("sessId", sessId);
+			reply.put("status", "ok");
 
 			return reply;
 		}
 
-		sessId = (String)msg.get( "sessId" );
-		long sessStart = (long)msg.get( "sessStart" );
-		String nsessId = getSessId( mac, userId, sessStart );
-		if( sessId.equals( nsessId ) )
-			msg.put( "status", "ok" );
-		else msg.put( "status", "error" );
+		sessId = (String) msg.get("sessId");
+		long sessStart = (long) msg.get("sessStart");
+		String nsessId = getSessId(mac, userId, sessStart);
+		if (sessId.equals(nsessId))
+			msg.put("status", "ok");
+		else
+			msg.put("status", "error");
 		return msg;
 	}
 
-	public static String getSessId( final Mac mac, String userId, long sessStart ) {
-		String cval = String.format("%s&%d", userId, sessStart );
+	public static String getSessId(final Mac mac, String userId, long sessStart)
+	{
+		String cval = String.format("%s&%d", userId, sessStart);
 		return new String(Base64.encodeBase64(mac.doFinal(cval.getBytes())));
 	}
 
 	@Override
-	protected void channelRead0(final ChannelHandlerContext ctx, final Request request ) throws Exception
+	protected void channelRead0(final ChannelHandlerContext ctx, final Request request) throws Exception
 	{
 		FullHttpRequest httpReq = request.getHttpRequest();
 		String path = request.getRequestPath();
 		HttpMethod accessMethod = httpReq.getMethod();
 
-        String cookiestr = httpReq.headers().get( HttpHeaders.Names.COOKIE );
-        Set< Cookie > cookies = nocookies;
-        if( cookiestr != null )
-            cookies = CookieDecoder.decode( cookiestr );
-		
+		String cookiestr = httpReq.headers().get(HttpHeaders.Names.COOKIE);
+		Set<Cookie> cookies = nocookies;
+		if (cookiestr != null)
+			cookies = CookieDecoder.decode(cookiestr);
+
 		logger.info("Intercepted msg : headers {} {} {}!!!", path, cookies);
 
 		Cookie sessIdc = null, userIdc = null, sessStartc = null;
-		for( Cookie c : cookies ) {
-			switch( c.getName()) {
-			case "sessId" :
+		for (Cookie c : cookies)
+		{
+			switch (c.getName())
+			{
+			case "sessId":
 				sessIdc = c;
 				break;
-			case "userId" :
+			case "userId":
 				userIdc = c;
 				break;
-			case "sessStart" :
+			case "sessStart":
 				sessStartc = c;
 				break;
-            default:
-                break;
+			default:
+				break;
 			}
 		}
 
 		String sessId = null, userId = null;
 		long sessStart = -1;
-        if (path.matches("/login") && accessMethod == HttpMethod.POST ) {
-            String body = httpReq.content().toString( CharsetUtil.UTF_8 );
+		if (path.matches("/login") && accessMethod == HttpMethod.POST)
+		{
+			String body = httpReq.content().toString(CharsetUtil.UTF_8);
 
 			logger.info("Login request: {}", path);
-			Map< String, String > credentials = Utils.om.readValue( body, new TypeReference< Map< String, String > >() {} );
-            userId = credentials.get("userId");
+			Map<String, String> credentials = Utils.om.readValue(body, new TypeReference<Map<String, String>>()
+			{
+			});
+			userId = credentials.get("userId");
 
-            if (userId == null) { // authenticate userId
-                logger.debug("Cannot validate User {}, Fix it, continuing as usual !" );
-                // return null;
-            }
-    
-            sessStart = System.currentTimeMillis() / 1000L;
-            sessId = getSessId( mac, userId, sessStart );
+			if (userId == null)
+			{ // authenticate userId
+				logger.debug("Cannot validate User {}, Fix it, continuing as usual !");
+				// return null;
+			}
 
-            FullHttpResponse resp = request.setResponse( HttpResponseStatus.OK, Unpooled.buffer(0) );
-            for( Cookie reqcookie : cookies ) {
-            	reqcookie.setMaxAge( 0 );
-            }
+			sessStart = System.currentTimeMillis() / 1000L;
+			sessId = getSessId(mac, userId, sessStart);
 
-            sessIdc = new DefaultCookie( "sessId", sessId );
-            sessIdc.setHttpOnly( true );
-            sessIdc.setPath("/");
-            cookies.remove( sessIdc );
+			FullHttpResponse resp = request.setResponse(HttpResponseStatus.OK, Unpooled.buffer(0));
+			for (Cookie reqcookie : cookies)
+			{
+				reqcookie.setMaxAge(0);
+			}
 
-            sessIdc.setMaxAge( sessStart + validityWindow );
-            cookies.add( sessIdc );
+			sessIdc = new DefaultCookie("sessId", sessId);
+			sessIdc.setHttpOnly(true);
+			sessIdc.setPath("/");
+			cookies.remove(sessIdc);
 
-            sessStartc = new DefaultCookie( "sessStart", Long.toString(sessStart) );
-            sessStartc.setHttpOnly( true );
-            sessStartc.setPath("/");
-            cookies.remove( sessStartc );
+			sessIdc.setMaxAge(sessStart + validityWindow);
+			cookies.add(sessIdc);
 
-            sessIdc.setMaxAge( sessStart + validityWindow );
-            cookies.add( sessStartc );
+			sessStartc = new DefaultCookie("sessStart", Long.toString(sessStart));
+			sessStartc.setHttpOnly(true);
+			sessStartc.setPath("/");
+			cookies.remove(sessStartc);
 
-            userIdc = new DefaultCookie( "userId", userId );
-            userIdc.setHttpOnly( true );
-            userIdc.setPath("/");
-            cookies.remove( userIdc );
+			sessIdc.setMaxAge(sessStart + validityWindow);
+			cookies.add(sessStartc);
 
-            userIdc.setMaxAge( sessStart + validityWindow );
-            cookies.add( userIdc );
+			userIdc = new DefaultCookie("userId", userId);
+			userIdc.setHttpOnly(true);
+			userIdc.setPath("/");
+			cookies.remove(userIdc);
 
-            resp.headers().set( HttpHeaders.Names.SET_COOKIE, ClientCookieEncoder.encode( cookies ) );
-            ctx.writeAndFlush( new FullEncodedResponse( request, resp ));
-            return;
-        }
-        
-        if (sessIdc == null && userIdc == null && sessStartc == null ) { // invalid request, opensession first
-			logger.error("Invalid request, session doesnt exist!");
-            FullHttpResponse resp = request.setResponse(HttpResponseStatus.UNAUTHORIZED, ctx.alloc().buffer().writeBytes("Authorization absent, kindly sign-in first".getBytes() ) );
-            ctx.writeAndFlush(new FullEncodedResponse( request, resp ));
+			userIdc.setMaxAge(sessStart + validityWindow);
+			cookies.add(userIdc);
+
+			resp.headers().set(HttpHeaders.Names.SET_COOKIE, ClientCookieEncoder.encode(cookies));
+			ctx.writeAndFlush(new FullEncodedResponse(request, resp));
 			return;
 		}
 
-        if( sessId == null )
-            sessId = sessIdc.getValue();
-		if( userId == null )
-            userId = userIdc.getValue();
-		if( sessStart < 0 )
-            sessStart = Long.valueOf( sessStartc.getValue() );
-		
-		String csessid = getSessId( mac, userId, sessStart );
+		if (sessIdc == null && userIdc == null && sessStartc == null)
+		{ // invalid request, opensession first
+			logger.error("Invalid request, session doesnt exist!");
+			FullHttpResponse resp = request.setResponse(HttpResponseStatus.UNAUTHORIZED, ctx.alloc().buffer().writeBytes("Authorization absent, kindly sign-in first".getBytes()));
+			ctx.writeAndFlush(new FullEncodedResponse(request, resp));
+			return;
+		}
+
+		if (sessId == null)
+			sessId = sessIdc.getValue();
+		if (userId == null)
+			userId = userIdc.getValue();
+		if (sessStart < 0)
+			sessStart = Long.valueOf(sessStartc.getValue());
+
+		String csessid = getSessId(mac, userId, sessStart);
 
 		if (!csessid.equals(sessId))
 		{
 			logger.error("Invalid credentials {} {}!!", sessId, csessid);
-			FullHttpResponse resp = request.setResponse(HttpResponseStatus.UNAUTHORIZED, ctx.alloc().buffer().writeBytes( "Unauthorized access: kindly sign-in again".getBytes() ) );
-            ctx.writeAndFlush(new FullEncodedResponse( request, resp ));
+			FullHttpResponse resp = request.setResponse(HttpResponseStatus.UNAUTHORIZED, ctx.alloc().buffer().writeBytes("Unauthorized access: kindly sign-in again".getBytes()));
+			ctx.writeAndFlush(new FullEncodedResponse(request, resp));
 			return;
 		}
 
 		long elapseSecs = System.currentTimeMillis() / 1000L - sessStart;
-		if (invalidSessions.contains(sessId) || elapseSecs > validityWindow )
+		if (invalidSessions.contains(sessId) || elapseSecs > validityWindow)
 		{
-			FullHttpResponse resp = request.setResponse(HttpResponseStatus.UNAUTHORIZED, ctx.alloc().buffer().writeBytes( String.format("Session Expired : %d", elapseSecs).getBytes() ) );
-            ctx.writeAndFlush(new FullEncodedResponse( request, resp ));
+			FullHttpResponse resp = request.setResponse(HttpResponseStatus.UNAUTHORIZED,
+					ctx.alloc().buffer().writeBytes(String.format("Session Expired : %d", elapseSecs).getBytes()));
+			ctx.writeAndFlush(new FullEncodedResponse(request, resp));
 			return;
 		}
 
-		if (path.matches("/logout") && accessMethod == HttpMethod.DELETE ) {
-            invalidSessions.add(sessId);
-            FullHttpResponse resp = request.setResponse(HttpResponseStatus.OK, ctx.alloc().buffer().writeBytes( "Session Deleted Successfully".getBytes() ) );
-            ctx.writeAndFlush( new FullEncodedResponse( request, resp ) );
-            return;
+		if (path.matches("/logout") && accessMethod == HttpMethod.DELETE)
+		{
+			invalidSessions.add(sessId);
+			FullHttpResponse resp = request.setResponse(HttpResponseStatus.OK, ctx.alloc().buffer().writeBytes("Session Deleted Successfully".getBytes()));
+			ctx.writeAndFlush(new FullEncodedResponse(request, resp));
+			return;
 		}
 
-		httpReq.headers().set("userId", userId );
-		httpReq.headers().set("sessStart", Long.toString( sessStart ) );
-		ctx.fireChannelRead( request );
+		httpReq.headers().set("userId", userId);
+		httpReq.headers().set("sessStart", Long.toString(sessStart));
+		ctx.fireChannelRead(request);
 	}
 }
